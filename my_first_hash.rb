96ec62f13d63be4b0ef7d8007d9d50b0c4517183
Joshua Sanders <ninjosh2@gmail.com>
2019-12-17 08:48:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,8 +4,17 @@ end
 
 
 
-def the_manifest = {"whale bone corsets" => 5 "porcelain vases" => 2 "oil paintings" => 3 "muskets" => 2 "gun powder" => 4}
- 
+def shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
-end 
+def retrieval
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
+end
+
+def adding
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
+end
