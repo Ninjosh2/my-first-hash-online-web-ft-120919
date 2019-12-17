8f4f2278c0d6d3b152d3c8a449c665ee7fd76c79
Joshua Sanders <ninjosh2@gmail.com>
2019-12-17 08:52:22 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,8 +2,6 @@ def my_hash
  my_hash = {"name" => "Joshy"} 
 end
 
-
-
 def shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
@@ -14,9 +12,6 @@ end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
   shipping_manifest["pearl necklace"] = 1
   shipping_manifest
 end
-end 
-end 
